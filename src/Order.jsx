f46d88143ef5665d0d49791fb758415330709609
Chanuka999 <chanukaranditha99@gmail.com>
2025-11-17 17:24:53 +0530

    feat: enhance Order component with dynamic pizza selection and loading state

--- src/Order.jsx
+++ src/Order.jsx
@@ -1,48 +1,84 @@
+import { useState, useEffect } from "react";
 import Pizza from "./Pizza";
 
-const Order = () => {
-  const pizzaType = "papperoni";
-  const pizzaSize = "M";
+// feel free to change en-US / USD to your locale
+const intl = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+});
+
+export default function Order() {
+  const [pizzaType, setPizzaType] = useState("pepperoni");
+  const [pizzaSize, setPizzaSize] = useState("M");
+  const [pizzaTypes, setPizzaTypes] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  let price, selectedPizza;
+  if (!loading) {
+    selectedPizza = pizzaTypes.find((pizza) => pizzaType === pizza.id);
+    price = intl.format(
+      selectedPizza.sizes ? selectedPizza.sizes[pizzaSize] : "",
+    );
+  }
+
+  useEffect(() => {
+    fetchPizzaTypes();
+  }, []);
+
+  async function fetchPizzaTypes() {
+    const pizzasRes = await fetch("/api/pizzas");
+    const pizzasJson = await pizzasRes.json();
+    setPizzaTypes(pizzasJson);
+    setLoading(false);
+  }
+
   return (
     <div className="order">
-      <h2>Create order</h2>
+      <h2>Create Order</h2>
       <form>
         <div>
           <div>
-            <label htmlFor="pizza-type">pizza Type</label>
-            <select name="pizza-type" value={pizzaType}>
-              <option value="papperoni">The papperoni pizza</option>
-              <option value="bbq">The BBQ Chicken pizza</option>
-              <option value="margherita">The Margherita pizza</option>
-              <option value="veggie">The Veggie Garden pizza</option>
+            <label htmlFor="pizza-type">Pizza Type</label>
+            <select
+              onChange={(e) => setPizzaType(e.target.value)}
+              name="pizza-type"
+              value={pizzaType}
+            >
+              {pizzaTypes.map((pizza, index) => (
+                <option key={index} value={pizza.id}>
+                  {pizza.name}
+                </option>
+              ))}
             </select>
           </div>
-
           <div>
-            <label htmlFor="pizza-size">Pizza size</label>
+            <label htmlFor="pizza-size">Pizza Size</label>
             <div>
               <span>
                 <input
-                  checked={pizzaSize === "s"}
+                  onChange={(e) => setPizzaSize(e.target.value)}
+                  checked={pizzaSize === "S"}
                   type="radio"
                   name="pizza-size"
-                  value="s"
+                  value="S"
                   id="pizza-s"
                 />
-                <label htmlFor="pizza-s">small</label>
+                <label htmlFor="pizza-s">Small</label>
               </span>
               <span>
                 <input
+                  onChange={(e) => setPizzaSize(e.target.value)}
                   checked={pizzaSize === "M"}
                   type="radio"
                   name="pizza-size"
                   value="M"
                   id="pizza-m"
                 />
-                <label htmlFor="pizza-s">Medium</label>
+                <label htmlFor="pizza-m">Medium</label>
               </span>
               <span>
                 <input
+                  onChange={(e) => setPizzaSize(e.target.value)}
                   checked={pizzaSize === "L"}
                   type="radio"
                   name="pizza-size"
@@ -55,17 +91,19 @@ const Order = () => {
           </div>
           <button type="submit">Add to Cart</button>
         </div>
-        <div className="order-pizzas">
-          <Pizza
-            name="pepperoni"
-            description="another pep pizza"
-            image="/public/pizzas/pepperoni.webp"
-          />
-          <p>$13.37</p>
-        </div>
+        {loading ? (
+          <h3>LOADING …</h3>
+        ) : (
+          <div className="order-pizza">
+            <Pizza
+              name={selectedPizza.name}
+              description={selectedPizza.description}
+              image={selectedPizza.image}
+            />
+            <p>{price}</p>
+          </div>
+        )}
       </form>
     </div>
   );
-};
-
-export default Order;
+}
