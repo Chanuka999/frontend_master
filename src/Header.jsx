80acd8509f9569cd3b097666bf855d8c7e83f719
Chanuka999 <chanukaranditha99@gmail.com>
2025-12-01 09:52:58 +0530

    feat: add Header component with navigation and cart display

---
+++ src/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <nav>
+      <h1 className="logo">chanuka randitha</h1>
+      <div className="nav-cart">
+        🛒<span className="nav-cart-number">5</span>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
