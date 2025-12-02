a222dbc294dee4a8955fea59cb7355038c760475
Chanuka999 <chanukaranditha99@gmail.com>
2025-12-02 10:32:18 +0530

    feat: refactor App component to remove unused imports and integrate routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,13 @@
 import React, { useState } from "react";
 import { createRoot } from "react-dom/client";
-<<<<<<< HEAD
-//import Pizza from "./Pizza.jsx";
-import Order from "./Order.jsx";
-import PizzaOfTheDay from "./PizzaOfTheDay.jsx";
-import { CartContext } from "./CartContext";
-import Header from "./Header.jsx";
-=======
+
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
 
 const router = createRouter({ routeTree });
->>>>>>> ccd566c (add)
 
 const App = () => {
   const cartHook = useState([]);
-
-<<<<<<< HEAD
-  return (
-    <CartContext.Provider value={cartHook}>
-      <div>
-        <Header />
-        <Order />
-
-        <PizzaOfTheDay />
-      </div>
-    </CartContext.Provider>
-  );
-=======
-  return <RouterProvider router={router} />;
->>>>>>> ccd566c (add)
 };
 
 const container = document.getElementById("root");
