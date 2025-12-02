4b2762f2c606733bd19991023208396620176a4a
Chanuka999 <chanukaranditha99@gmail.com>
2025-12-02 10:28:10 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import React, { useState } from "react";
 import { createRoot } from "react-dom/client";
+<<<<<<< HEAD
 //import Pizza from "./Pizza.jsx";
 import Order from "./Order.jsx";
 import PizzaOfTheDay from "./PizzaOfTheDay.jsx";
 import { CartContext } from "./CartContext";
 import Header from "./Header.jsx";
+=======
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { routeTree } from "./routeTree.gen";
+
+const router = createRouter({ routeTree });
+>>>>>>> ccd566c (add)
 
 const App = () => {
   const cartHook = useState([]);
 
+<<<<<<< HEAD
   return (
     <CartContext.Provider value={cartHook}>
       <div>
@@ -19,6 +27,9 @@ const App = () => {
       </div>
     </CartContext.Provider>
   );
+=======
+  return <RouterProvider router={router} />;
+>>>>>>> ccd566c (add)
 };
 
 const container = document.getElementById("root");
